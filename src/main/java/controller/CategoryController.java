9ded6a961ad5f166ceb8a1b48c65ba07be57820a
Lucas <lucasgomesmorais58@gmail.com>
2024-09-14 21:20:24 -0300

    add controller with REST endpoints

--- src/main/java/controller/CategoryController.java
+++ src/main/java/controller/CategoryController.java
@@ -1,2 +1,38 @@
-package controller;public class CategoryController {
+package controller;
+
+import dto.CategoryDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.CategoryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        List<CategoryDTO> categories = categoryService.findAll();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable String id) {
+        CategoryDTO category = categoryService.findById(id);
+        return ResponseEntity.ok(category);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO createdCategory = categoryService.create(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
+    }
 }
