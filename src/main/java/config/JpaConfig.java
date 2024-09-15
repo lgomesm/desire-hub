1920fbb400e3a3b671a59ebd70d5b0e764877398
Lucas <lucasgomesmorais58@gmail.com>
2024-09-14 21:16:15 -0300

    chore(config): configure JPA

--- src/main/java/config/JpaConfig.java
+++ src/main/java/config/JpaConfig.java
@@ -1,2 +1,9 @@
-package config;public class JpaConfig {
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
 }
