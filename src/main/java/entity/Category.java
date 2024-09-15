3b9cd69168184dddd20c01ff00bcb80ffc171f09
Lucas <lucasgomesmorais58@gmail.com>
2024-09-14 21:16:50 -0300

    feat(entity): add category class

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -1,2 +1,53 @@
-package entity;public class Category {
+package entity;
+
+import dto.CategoryDTO;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@EntityListeners(AuditingEntityListener.class)
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @NotBlank(message = "Category name cannot be null or empty")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    private Category(@NotNull final String name) {
+        this.name = name;
+    }
+
+    public static Category fromDTO(CategoryDTO dto) {
+        return new Category(dto.getName());
+    }
+
+    public CategoryDTO toDTO() {
+        return new CategoryDTO(id != null ? id.toString() : null, name);
+    }
 }
